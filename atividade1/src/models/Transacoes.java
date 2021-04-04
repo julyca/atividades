9ccd987a73fcbc2e6d0bb76554a8b9aca8f67999
Juliana-Haroldo <79457732+Juliana-Haroldo@users.noreply.github.com>
2021-04-04 20:20:36 -0300

    implementação classe transacoes

--- atividade1/src/models/Transacoes.java
+++ atividade1/src/models/Transacoes.java
@@ -1,5 +1,39 @@
 package models;
 
+import java.util.Random;
+
+import controls.Seguranca;
+
 public class Transacoes {
+
+    private static int getnumQRcode() {
+        Random r = new Random();
+        return r.nextInt(9999 - 1000) + 1000;
     
+    }
+
+    public String GeradorQRcode(Usuario usuario, double valor){
+        Seguranca seg = new Seguranca();
+        int numTransacao = getnumQRcode();
+        return seg.getContaUsuario(usuario.getId()) + "; " + usuario.getNome() + "; " + valor + "; " + numTransacao;
+
+    }
+
+    public void pagamentoQRcode(Usuario remetente, Usuario destinatario, String QRcode){
+        Seguranca seg = new Seguranca();
+        String array[] = new String[4];
+        array = QRcode.split(";");
+        double valor = Double.parseDouble(array[2]);
+
+        if (seg.getContaUsuario(remetente.getId()).sacar(valor)){
+            seg.getContaUsuario(destinatario.getId()).depositar(valor);
+            System.out.println("Transação realizada com sucesso!");
+        } 
+        
+        else System.out.println("Transação não realizada por falta de saldo");
+
+    }
+
 }
+
+
